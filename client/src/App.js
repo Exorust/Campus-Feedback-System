79a889ed56e0560e6cc9b7060085d25821702199
gopeshkh1 <f20160101@hyderabad.bits-pilani.ac.in>
2019-03-01 23:34:56 +0530

    Edits

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/auth/login";
+import Navbar from "./components/layout/Navbar";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Login />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Login />
+        </div>
+      </Router>
     );
   }
 }
